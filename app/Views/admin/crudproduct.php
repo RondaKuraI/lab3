c47f22cb0dfda4e10252455f4af6760780dda90c
RondaKuraI <johnronda0527@gmail.com>
2023-10-01 22:20:05 +0800

    Update crudproduct.php

--- app/Views/admin/crudproduct.php
+++ app/Views/admin/crudproduct.php
@@ -7,17 +7,90 @@
     <?= $this->include('include/sidenav') ?>
 
     <div id="layoutSidenav_content">
-    <main>
+                <main>
+                    
                     <div class="container-fluid px-4">
-                        <h1 class="mt-4">Charts</h1>
+                        <h1 class="mt-4">Products</h1>
                         <ol class="breadcrumb mb-4">
-                            <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
-                            <li class="breadcrumb-item active">Charts</li>
+                            <li class="breadcrumb-item"><a href="/">Dashboard</a></li>
+                            <li class="breadcrumb-item active">Products</li>
                         </ol>
-                        
-
-                        
                     </div>
+
+
+                        <!-- Modal -->
+                        <div class="modal fade" id="productModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <form action="" method="POST">
+                                <div class="modal-body">
+                                    <div class="mb-3">
+                                        <label for="exampleInputPassword1" class="form-label">Password</label>
+                                        <input type="password" class="form-control" id="exampleInputPassword1">
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary">Save changes</button>
+                                </div>
+                            </form>
+                            </div>
+                        </div>
+                        </div>
+                    
+                    <div class="container-fluid">
+                        <div class="card shadow mb-4">
+                            <div class="card-header py-3">
+                                <h6 class="m-0 font-weight-bold text-primary">Products
+                                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#productModal">
+                                        ADD
+                                    </button>
+                                </h6>
+                            </div>
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                        <thead>
+                                            <tr>
+                                                <th>ID</th>
+                                                <th>Name</th>
+                                                <th>Description</th>
+                                                <th>Image</th>
+                                                <th>Price</th>
+                                                <th>Category</th>
+                                                <th>Quantity</th>
+                                                <th>Edit</th>
+                                                <th>Delete</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>ID</td>
+                                                <td>Name</td>
+                                                <td>Description</td>
+                                                <td>Image</td>
+                                                <td>Price</td>
+                                                <td>Category</td>
+                                                <td>Quantity</td>
+                                                <td><a href="#" class="btn btn-success">Edit</a></td>
+                                                <td><a href="#" class="btn btn-danger">Delete</a></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+
+
                 </main>
                 <footer class="py-4 bg-light mt-auto">
                     <div class="container-fluid px-4">
@@ -37,3 +110,5 @@
     
 
 <?= $this->include('include/end') ?>
+
+
